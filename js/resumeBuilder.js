51704ada2d6cca25b6f2d771a1db0e8ef0d9e0d3
luke <luke>
2017-06-01 18:21:01 +0800

    modify luke JSON data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,74 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var bio = {
+  "name": "LUKE Bei",
+  "role": "Full Stack Developer",
+  "contacts": {
+    "mobile": "17698098378",
+    "email": "luke.bei.2015@gmail.com",
+    "github": "luke",
+    "twitter": "@lukebei",
+    "location": "Zhengzhou"
+  },
+  "welcomeMessage": "Hey, This is luke",
+  "skills": [
+    "awesomeness", "delivering things", "docker", "Frontend"
+  ],
+  "biopic": "images/fry.jpg"
+};
+
+var education = {
+  schools: [{
+      "name": "Zhengzhou University Of Light Industry",
+      "city": "Zhengzhou, China",
+      "degree": "Bechalar",
+      "majors": ["CS"],
+      "dates": 2014,
+      "url": "http://zzuli.edu.cn"
+    },
+    {
+      "name": "Zhengzhou University Of Light Industry",
+      "city": "Zhengzhou, China",
+      "degree": "Bechalar",
+      "majors": ["CS"],
+      "dates": 2014,
+      "url": "http://zzuli.edu.cn"
+    }
+  ],
+  "onlineCourse": [{
+    "title": "JavaScript Crash Course",
+    "school": "Udacity",
+    "dates": 2014,
+    "url": "https://cn.udacity.com/course/javascript-basics--ud804/"
+  }]
+};
+
+var work = {
+  "jobs" : [
+    {
+      "employer" : "ZZULI",
+      "title" : "PM",
+      "dates" : "2016.3",
+      "description" : "在爱丁堡龙比亚大学Merchiston校区的古老城堡里，我校学生与英方六名学生进行了为期两周的项目研讨，从各自课题阐述、辩论到确定小组研究课题，小组成员进行了大量细致深入的讨论和分析，并对该项目未来的各阶段工作进行安排。在离开英国前，IGP小组学生向龙比亚大学的专家组就两周的成果和规划做了精彩的展示。在未来的三个月内，双方将通过互联网进行远程科研项目合作和研究，对该项目进行实质性地展开与推进，预计五月中旬完成。届时，爱丁堡龙比亚大学的六名学生将会前来我校共同完成最后的项目总结报告。"
+    },
+    {
+      "employer" : "ZZULI",
+      "title" : "PM",
+      "dates" : "2016.8",
+      "description" : "在爱丁堡龙比亚大学Merchiston校区的古老城堡里，我校学生与英方六名学生进行了为期两周的项目研讨，从各自课题阐述、辩论到确定小组研究课题，小组成员进行了大量细致深入的讨论和分析，并对该项目未来的各阶段工作进行安排。在离开英国前，IGP小组学生向龙比亚大学的专家组就两周的成果和规划做了精彩的展示。在未来的三个月内，双方将通过互联网进行远程科研项目合作和研究，对该项目进行实质性地展开与推进，预计五月中旬完成。届时，爱丁堡龙比亚大学的六名学生将会前来我校共同完成最后的项目总结报告。"
+    }
+  ]
+};
+
+var projects = {
+  "projects" : [
+    {
+      "title" : "ZZULI IGP project",
+      "dates" : "2014",
+      "description" : "在爱丁堡龙比亚大学Merchiston校区的古老城堡里，我校学生与英方六名学生进行了为期两周的项目研讨，从各自课题阐述、辩论到确定小组研究课题，小组成员进行了大量细致深入的讨论和分析，并对该项目未来的各阶段工作进行安排。在离开英国前，IGP小组学生向龙比亚大学的专家组就两周的成果和规划做了精彩的展示。在未来的三个月内，双方将通过互联网进行远程科研项目合作和研究，对该项目进行实质性地展开与推进，预计五月中旬完成。届时，爱丁堡龙比亚大学的六名学生将会前来我校共同完成最后的项目总结报告。",
+      "images" : ["http://iec.zzuli.edu.cn/picture/article/103/13/a6/b00511e74b0b8a51c17f33c24a59/94765e44-6cee-4405-a5d0-2816e5c3df98.jpg","http://iec.zzuli.edu.cn/picture/article/103/13/a6/b00511e74b0b8a51c17f33c24a59/071d1d52-b735-484e-ae79-b778ff6545ae.jpg"]
+    }
+  ]
+};
